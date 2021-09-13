611917b31e2a50c074413ab0a375092aa22c2f02
Đào Thanh Tùng <tungdt2@bambooairways.com>
2021-09-13 13:35:29 +0700

    update

--- app/Console/Commands/SyncData/PaymentOrder.php
+++ app/Console/Commands/SyncData/PaymentOrder.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands\SyncData;
 
+use App\Models\PaymentOrder\PaymentOrder as PaymentOrderPaymentOrder;
 use App\Models\SyncData\AlilogiTransportOrder;
+use App\Models\TransportOrder\TransportCode;
 use Illuminate\Console\Command;
 
 class PaymentOrder extends Command
@@ -38,11 +40,44 @@ class PaymentOrder extends Command
      */
     public function handle()
     {
-        $transportOrders = AlilogiTransportOrder::all();
+        $transportOrders = AlilogiTransportOrder::orderBy('id', 'desc')->get();
 
         foreach ($transportOrders as $order) {
-            dd($order);
+            echo $order->order_number . "\n";
+            $data = [
+                'order_number'  =>  $order->order_number,
+                'status'    =>  "payment_export",
+                'amount'    =>  str_replace(",", "", number_format($order->final_total_price, 0)),
+                'total_kg'  =>  $order->transport_kg,
+                'total_m3'  =>  $order->transport_cublic_meter,
+                'total_v'   =>  $order->transport_volume,
+                'total_advance_drag'    =>  $order->transport_advance_drag,
+                'user_created_id'   =>  $order->user_created_id,
+                'payment_customer_id'   =>  $order->payment_customer_id,
+                'internal_note' =>  $order->internal_note,
+                'discount_value'    =>  $order->discount_value,
+                'discount_type' =>  $order->discount_type,
+                'price_kg'  =>  $this->getPriceService(1, $order->id),
+                'price_m3'    =>  $this->getPriceService(-1, $order->id),
+                'price_v'     =>  $this->getPriceService(0, $order->id),
+                'is_sub_customer_wallet_weight' =>  0,
+                'total_sub_wallet_weight'   =>  0,
+                'current_rate'  =>  $order->current_rate,
+                'transaction_note'  =>  null,
+                'export_at' =>  $order->created_at,
+                'user_export_id'    =>  $order->user_created_id,
+                'owed_purchase_order'   =>  0,
+                'purchase_order_id' =>  null,
+                '_id'   =>  $order->id
+            ];
+
+            PaymentOrderPaymentOrder::firstOrCreate($data);
         }
 
+    }
+
+    public function getPriceService($type, $orderId) {
+        $flag = TransportCode::where('order_id', $orderId)->where('payment_type', $type)->first();
+        return $flag ? $flag->price_service : 0;
     }
 }
