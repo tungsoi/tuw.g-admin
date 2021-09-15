553cfff12cd1f87fa62a760201787c64201fa85a
Đào Thanh Tùng <tungdt2@bambooairways.com>
2021-09-15 13:35:02 +0700

    update

--- app/Admin/Controllers/TransportOrder/TransportCodeController.php
+++ app/Admin/Controllers/TransportOrder/TransportCodeController.php
@@ -38,6 +38,7 @@ class TransportCodeController extends AdminController
         $grid = new Grid(new TransportCode());
         $grid->model()->where('transport_code', '!=', "")
             ->orderBy('vietnam_receive_at', 'desc')
+            ->orderBy('payment', 'asc')
             ->orderBy('export_at', 'asc')
             ->orderBy('customer_code_input', 'desc');
         
