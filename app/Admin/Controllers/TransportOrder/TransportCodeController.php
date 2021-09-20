e776530d33d37a0ff533280ade854928e3911366
Đào Thanh Tùng <tungdt2@bambooairways.com>
2021-09-20 11:08:32 +0700

    update

--- app/Admin/Controllers/TransportOrder/TransportCodeController.php
+++ app/Admin/Controllers/TransportOrder/TransportCodeController.php
@@ -69,7 +69,7 @@ class TransportCodeController extends AdminController
                 
             });
             $filter->column(1/4, function ($filter) use ($orderService)  {
-                $filter->equal('customer_code_input', 'Mã khách hàng');
+                $filter->like('customer_code_input', 'Mã khách hàng');
                 $filter->equal('status', 'Trạng thái')->select(TransportCodeStatus::pluck('name', 'id'));
             });
 
