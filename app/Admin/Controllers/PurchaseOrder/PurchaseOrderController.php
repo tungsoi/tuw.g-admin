e033346c482760d6dab4d9754b86e46f648853ec
Đào Thanh Tùng <tungdt2@bambooairways.com>
2021-09-23 13:01:30 +0700

    update

--- app/Admin/Controllers/PurchaseOrder/PurchaseOrderController.php
+++ app/Admin/Controllers/PurchaseOrder/PurchaseOrderController.php
@@ -145,6 +145,10 @@ class PurchaseOrderController extends AdminController
             ');
         });
 
+        $grid->header(function () {
+            return "<h4>Tổng tiền cọc dự tính: <span id='estimate-deposited' style='color: red; font-weight: 700;'></span> (VND) </h4>";
+        });
+        
         $grid->rows(function (Grid\Row $row) {
             $row->column('number', ($row->number+1));
         });
@@ -270,7 +274,7 @@ class PurchaseOrderController extends AdminController
                 ],
                 'deposite'  =>  [
                     'is_label'  =>  false,
-                    'text'      =>  "<i style='color: red'>Cần cọc: ". number_format($deposite) . "</i>"
+                    'text'      =>  "<i style='color: red'>Cần cọc: <span class='default-deposite'>". number_format($deposite) . "</span></i>"
                 ]
             ];            
             return view('admin.system.core.list', compact('data'));
@@ -431,7 +435,67 @@ EOT);
             }
         });
 
+        Admin::script($this->scriptGrid());
+
         return $grid;
+    }
+
+    public function scriptGrid() {
+        return <<<SCRIPT
+        console.log('grid js');
+
+        $("input.grid-row-checkbox").on("ifChanged", function () {
+
+            let total_deposite = 0;
+            var key = $.admin.grid.selected();
+        
+            if (key.length !== 0) {
+                var i;
+                for (i = 0; i < key.length; ++i) {
+                    let data_key = key[i];
+
+                    let tr_ele = $('tr[data-key="'+data_key+'"]');
+                    let default_deposite = tr_ele.find('.default-deposite').html();
+                    default_deposite = default_deposite.replace(/,/g, "");
+                    default_deposite = parseInt(default_deposite);
+
+                    total_deposite += default_deposite;
+                }
+            }
+
+            let total_deposite_formated = number_format(total_deposite);
+
+            $('#estimate-deposited').html(total_deposite_formated);
+            
+        });
+
+        function number_format(number, decimals, dec_point, thousands_sep) {
+            // Strip all characters but numerical ones.
+            number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+            var n = !isFinite(+number) ? 0 : +number,
+                prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+                sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+                dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+                s = '',
+                toFixedFix = function (n, prec) {
+                    var k = Math.pow(10, prec);
+                    return '' + Math.round(n * k) / k;
+                };
+            // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+            s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+            if (s[0].length > 3) {
+                s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+            }
+            if ((s[1] || '').length < prec) {
+                s[1] = s[1] || '';
+                s[1] += new Array(prec - s[1].length + 1).join('0');
+            }
+            return s.join(dec);
+        }
+
+        
+ 
+SCRIPT;
     }
 
     /**
