51f3076f6ebb26ed3aeb833c5d46391e5a6de976
Đào Thanh Tùng <tungdt2@bambooairways.com>
2021-10-02 16:11:05 +0700

    update

--- resources/views/admin/system/purchase_order/customer_has_new_order.blade.php
+++ resources/views/admin/system/purchase_order/customer_has_new_order.blade.php
@@ -1,6 +1,6 @@
-<div class="col-md-12" style="border: 1px solid black; padding: 10px; text-transform: uppercase;" >{{ $title }}</div>
+<div class="col-md-12" style="border: 1px solid #d2d6de; padding: 10px; text-transform: uppercase;" ><b>{{ $title }}</b></div>
 @foreach ($customers as $key => $customer)
-    <div class="col-md-2" style="border: 1px solid black; padding: 10px; margin: 1px; width: 16.5%">
+    <div class="col-md-2" style="border: 1px solid #d2d6de; padding: 10px; margin: 1px; width: 16.5%">
         <a target="_blank" href="{{ route('admin.purchase_orders.index') }}?customer_id={{ $customer->customer->id }}&status={{ $status }}">
             {{ ($key+1) .". ". $customer->customer->symbol_name }}
         </a>
