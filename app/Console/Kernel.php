76da93939f6465e16d4615db941a8eeedd0f4fa3
Đào Thanh Tùng <tungdt2@bambooairways.com>
2021-10-28 15:22:37 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('report-warehouse:portal')->everyFiveMinutes();
         $schedule->command('purchase-order:delete-non-item')->everyMinute();
         $schedule->command('submit:success-order')->everyMinute();
-        $schedule->command('delete:error-transport-code-china')->everyFiveMinutes();
+        $schedule->command('delete:error-transport-code-china')->everyMinute();
         $schedule->command('payment_order:fill_export_at')->hourly();
 
         $schedule->command('sale-revenue-report:update', ['2021-10-01', '2021-10-31'])->dailyAt("13:00");
