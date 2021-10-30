b1f0f6e75d91144328151ba36dff7317b2178176
Đào Thanh Tùng <tungdt2@bambooairways.com>
2021-10-30 11:46:00 +0700

    update

--- resources/views/admin/system/purchase_order/search_items.blade.php
+++ resources/views/admin/system/purchase_order/search_items.blade.php
@@ -35,7 +35,7 @@
         <td>{{ $item->purchase_cn_transport_fee }}</td>
         <td>---</td>
         <td>
-            <span class="status">{{ $item->statusText->name }}</span>
+            <span class="status" @if($item->status == 3) style='color: green' @endif>{{ $item->statusText->name }}</span>
         </td>
         <td>
             @if ($item->status != 3)
