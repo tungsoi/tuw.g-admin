12b4af012ee975168f061b129ae04d6783a78eac
Đào Thanh Tùng <tungdt2@bambooairways.com>
2021-12-08 15:06:16 +0700

    update

--- resources/views/admin/system/report_portal/sale_salary.blade.php
+++ resources/views/admin/system/report_portal/sale_salary.blade.php
@@ -17,7 +17,7 @@
     <div class="row">
         <div class="col-md-12">
             <h5>{{ $report->title }}</h5>
-            <h5>Thời gian cập nhật: <span class="text-red">{{ $report->updated_at }}</span></h5>
+            <h5>Thời gian cập nhật: <span class="text-red">{{ $data->last()->updated_at }}</span></h5>
             <h5>ĐƠN HÀNG ORDER HOÀN THÀNH: <span class="text-red">Tính theo ngày thành công trong tháng</span> </h5>
             <h5>ĐƠN HÀNG VẬN CHUYỂN: <span class="text-red">Tính theo đơn hàng xuất kho trong tháng</span></h5>
         </div>
@@ -43,24 +43,25 @@
             <td>
                 <ul>
                     <li>Khách hàng: <span class="pull-right">{{ $data->sum('all_customer') }}</span></li>
-                    <li>Tổng âm ví: <span class="pull-right">{{ number_format($data->sum('owed_wallet_all_customer')) }}</span></li>
+                    <li>Tổng âm ví: <span class="pull-right text-red">{{ number_format($data->sum('owed_wallet_all_customer')) }}</span></li>
                 </ul>
             </td>
             <td>
                 <ul>
                     <li>Tổng đơn: <span class="pull-right">{{ $data->sum('po_success') }}</span></li>
-                    <li>Tổng DS: <span class="pull-right">{{ number_format($data->sum('po_success_all_customer')) }}</span></li>
-                    <li>Tổng PDV: <span class="pull-right">{{ number_format($data->sum('po_success_service_fee')) }}</span></li>
-                    <li>Tổng đàm phán: <span class="pull-right">{{ number_format($data->sum('po_success_offer')) }}</span></li>
+                    <li>Tổng DS: <span class="pull-right text-red">{{ number_format($data->sum('po_success_all_customer')) }}</span></li>
+                    <li>Tổng PDV: <span class="pull-right text-red">{{ number_format($data->sum('po_success_service_fee')) }}</span></li>
+                    <li>Tổng đàm phán: <span class="pull-right text-red">{{ number_format($data->sum('po_success_offer')) }}</span></li>
+                    <li>Tổng tệ: <span class="pull-right text-red">{{ number_format($data->sum('po_success_total_rmb')) }}</span></li>
                 </ul>
             </td>
             <td>
                 <ul>
                     <li>Tổng đơn: <span class="pull-right">{{ $data->sum('po_not_success') }}</span></li>
                     <li>Tổng DS: <span class="pull-right">{{ number_format($data->sum('po_not_success_all_customer')) }}</span></li>
-                    <li>Tổng PDV: <span class="pull-right">{{ number_format($data->sum('po_not_success_service_fee')) }}</span></li>
+                    <li>Tổng PDV: <span class="pull-right text-red">{{ number_format($data->sum('po_not_success_service_fee')) }}</span></li>
                     <li>Tổng cọc: <span class="pull-right">{{ number_format($data->sum('po_not_success_deposited')) }}</span></li>
-                    <li>Tổng công nợ: <span class="pull-right">{{ number_format($data->sum('po_not_success_owed')) }}</span></li>
+                    <li>Tổng công nợ: <span class="pull-right text-red">{{ number_format($data->sum('po_not_success_owed')) }}</span></li>
                 </ul>
             </td>
             <td>
@@ -68,10 +69,28 @@
                     <li>Tổng đơn: <span class="pull-right">{{ $data->sum('transport_order') }}</span></li>
                     <li>Tổng KG: <span class="pull-right">{{ $data->sum('trs_kg_all_customer') }}</span></li>
                     <li>Tổng M3: <span class="pull-right">{{ $data->sum('trs_m3_all_customer') }}</span></li>
-                    <li>Tổng DT: <span class="pull-right">{{ number_format($data->sum('trs_amount_all_customer')) }}</span></li>
+                    <li>Tổng DT: <span class="pull-right text-red">{{ number_format($data->sum('trs_amount_all_customer')) }}</span></li>
                 </ul>
             </td>
-            <td></td>
+            <td>
+                <ul>
+                    <li class="po_success">Số đơn TC <span class="pull-right">{{ $data->sum('po_success') }}</span></li>
+                    <li class="po_success_service_fee">Phí dịch vụ <i>(100%)</i> <span class="pull-right">{{ number_format($data->sum('po_success_service_fee')) }}</span></li>
+                    <li class="trs_amount_all_customer">DT vận chuyển <i>(10%)</i><span class="pull-right">{{ number_format($data->sum('trs_amount_all_customer')*0.1) }}</span></li>
+                    <li class="po_success_total_rmb">DT tỷ giá <i>(30 * Tổng giá tệ)</i> <span class="pull-right">{{ number_format($data->sum('po_success_total_rmb')*30) }}</span></li>
+                    <li class="po_success_offer">DT đàm phán <i>(85%)</i> <span class="pull-right">{{ number_format($data->sum('po_success_offer')*0.85) }}</span></li>
+                    <li class="total_revenue">
+                        Tổng doanh thu
+                        @php
+                            $total = $data->sum('po_success_service_fee') 
+                            + ($data->sum('trs_amount_all_customer')*0.1) 
+                            + ($data->sum('po_success_total_rmb')*30) 
+                            + ($data->sum('po_success_offer')*0.85);
+                        @endphp
+                        <span class="pull-right text-red">{{ number_format($total) }}</span>
+                    </li>
+                </ul>
+            </td>
         </tr>
         @foreach ($data as $key => $value)
         <tr>
