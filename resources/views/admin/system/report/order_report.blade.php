aee87948071013bd3ca38299dfd68ab0d2c8024e
Đào Thanh Tùng <tungdt2@bambooairways.com>
2022-01-05 13:52:30 +0700

    update

--- resources/views/admin/system/report/order_report.blade.php
+++ resources/views/admin/system/report/order_report.blade.php
@@ -10,8 +10,11 @@
         <th style="background: wheat !important">Tổng thực đặt</th>
     </thead>
     <tbody>
+        @php
+            $user_name = $number = $amount = $final_payment = $percent_service = $offer_cn = $offer_vn = $total = $index = 0;
+        @endphp
         @if ($data != null)
-            @foreach ($data as $user)
+            @foreach ($data as $key => $user)
                 <tr>
                     <td>{{ $user->user_name }}</td>
                     <td>{{ $user->number }}</td>
@@ -22,7 +25,27 @@
                     <td>{{ number_format($user->offer_vn) }}</td>
                     <td>{{ $user->total }}</td>
                 </tr>
+                @php
+                    $index ++;
+                    $number += $user->number;
+                    $amount += $user->amount;
+                    $final_payment += $user->final_payment;
+                    $percent_service += $user->percent_service;
+                    $offer_cn += $user->offer_cn;
+                    $offer_vn += $user->offer_vn;
+                    $total += $user->total;
+                @endphp
             @endforeach
+            <tr>
+                <td style="background: lavender !important">Tổng: {{ $index }}</td>
+                <td style="background: lavender !important">{{ $number }}</td>
+                <td style="background: lavender !important">{{ $amount }}</td>
+                <td style="background: lavender !important">{{ $final_payment }}</td>
+                <td style="background: lavender !important">{{ $percent_service }}</td>
+                <td style="background: lavender !important">{{ $offer_cn }}</td>
+                <td style="background: lavender !important">{{ number_format($offer_vn) }}</td>
+                <td style="background: lavender !important">{{ $total }}</td>
+            </tr>
         @else
             <tr>
                 <td colspan="8"><i>Không có dữ liệu</i></td>
